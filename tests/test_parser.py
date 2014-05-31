bfca3737183898094d2e28814c56448a1cb06155
Tathagata <tathagatadg@gmail.com>
2014-05-31 07:06:34 -0500

    Added basic structure for nose tests, travis
    Also modified .gitignore to not include the .dependency_list

---
+++ tests/test_parser.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestParser(unittest.TestCase):
+    def setUp(self):
+        pass
+    
+    def tearDown(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/test_print_dependency_tree.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestPrintDependencyTree(unittest.TestCase):
+    def setUp(self):
+        pass
+    
+    def tearDown(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
